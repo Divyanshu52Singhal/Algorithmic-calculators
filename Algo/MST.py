7057898d302ff21ec0d4c819d55235597fa75f26
Divyanshu <divyanshu52singhal@gmail.com>
2023-02-14 20:15:23 +0530

    MST completed

--- Algo/MST.py
+++ Algo/MST.py
@@ -2,6 +2,18 @@ import streamlit as st
 import networkx as nx
 import matplotlib.pyplot as plt
 
+page_bg_img = """
+    <style>
+    [data-testid="stAppViewContainer"]{
+    background-color:#E5B8F4;
+    padding:none;
+    margin:none;
+    background-image: linear-gradient(0deg,#E5B8F4 ,#810CA8 );
+    }
+    </style>
+    """
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 def find(parent, i):
     if parent[i] == i:
