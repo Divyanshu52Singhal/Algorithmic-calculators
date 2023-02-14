9027e0fb6bc10018d0e0c8ba8abe2a965484fe36
Divyanshu Kumar Singhal <98414032+Divyanshu81@users.noreply.github.com>
2023-02-14 19:15:38 +0530

    Update lcs.py

--- Algo/lcs.py
+++ Algo/lcs.py
@@ -1,38 +1,48 @@
+import streamlit as st
 
-def lcs(X, Y, m, n):
-    L = [[0 for i in range(n+1)] for j in range(m+1)]
+page_bg_img = """
+    <style>
+    [data-testid="stAppViewContainer"]{
+    background-color:#E5B8F4;
+    padding:none;
+    margin:none;
+    background-image: linear-gradient(0deg,#E5B8F4 ,#810CA8 );
+    }
+    </style>
+    """
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
-    for i in range(m+1):
-        for j in range(n+1):
+def lcs(x, y):
+    m = len(x)
+    n = len(y)
+    # Create a matrix to store the lengths of LCS of substrings of x and y
+    lcs_lengths = [[0] * (n + 1) for _ in range(m + 1)]
+    # Fill the matrix in bottom-up manner
+    for i in range(m + 1):
+        for j in range(n + 1):
             if i == 0 or j == 0:
-                L[i][j] = 0
-            elif X[i-1] == Y[j-1]:
-                L[i][j] = L[i-1][j-1] + 1
+                lcs_lengths[i][j] = 0
+            elif x[i-1] == y[j-1]:
+                lcs_lengths[i][j] = lcs_lengths[i-1][j-1] + 1
             else:
-                L[i][j] = max(L[i-1][j], L[i][j-1])
-
+                lcs_lengths[i][j] = max(lcs_lengths[i-1][j], lcs_lengths[i][j-1])
+    # Traverse the matrix to find the LCS
     lcs = ""
-    i = m
-    j = n
+    i, j = m, n
     while i > 0 and j > 0:
-        if X[i-1] == Y[j-1]:
-            lcs += X[i-1]
-            print(i,j,X[i-1])
+        if x[i-1] == y[j-1]:
+            lcs = x[i-1] + lcs
             i -= 1
             j -= 1
-        elif L[i-1][j] > L[i][j-1]:
+        elif lcs_lengths[i-1][j] > lcs_lengths[i][j-1]:
             i -= 1
         else:
             j -= 1
+    return lcs
 
-    lcs = lcs[::-1]
-    print("LCS of " + X + " and " + Y + " is " + lcs)
-
-
-X = input("ENTER FIRST STRING :")
-Y = input("ENTER SECOND STRING :")
-m = len(X)
-n = len(Y)
-lcs(X, Y, m, n)
-
-# This code is contributed by AMAN ASATI
+st.title("Longest Common Subsequence Finder")
+x = st.text_input("Enter the first string:")
+y = st.text_input("Enter the second string:")
+if st.button("Find LCS"):
+    lcs = lcs(x, y)
+    st.write(f"<b><h3>The longest common subsequence of : \n\n<b><h3> '{x}' \n\n <b><h3> AND <b>\n\n <b><h3>'{y}' \n\n<b><h3> IS \n\n<b><h3>'{lcs}'", unsafe_allow_html=True)
